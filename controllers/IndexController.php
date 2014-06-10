80af7f5d9163510508279b1e645401934233d3b6
Matthew Butler <matthewtbutler@gmail.com>
2014-06-10 11:37:48 -0500

    error handling for item/file params
    makes sure both of the item and file params passed in correspond to real
    objects and that the item and files are correlated

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -12,18 +12,33 @@ class Scriptus_IndexController extends Omeka_Controller_AbstractActionController
     
     public function transcribeAction()
     {
-        //$itemId = $this->getParam('item');
+        $transcription = '';
+
+        $itemId = $this->getParam('item');
         $fileId = $this->getParam('file');
-        //$item = get_record_by_id('item', $itemId);
-        $file = get_record_by_id('file', $fileId);
-        $filename = $file['filename'];      
-        set_current_record('file', $file);   
-        $imageUrl = $file->getWebPath('original');   
-        $metadata = metadata($file, array('Scripto', 'Transcription'));   
+        
+        $item = get_record_by_id('item', $itemId);
+        $file = get_record_by_id('file', $fileId);       
+
+        if (!$item || !$file) {
+
+            throw new Zend_Controller_Action_Exception('This page does not exist', 404);  
+
+        } elseif ($file->item_id != $itemId) {
+
+            throw new Zend_Controller_Action_Exception('This page does not exist', 404);             
+
+        } else {
+
+           $filename = $file['filename'];      
+           set_current_record('file', $file);   
+           $imageUrl = $file->getWebPath('original');
+           $transcription = metadata($file, array('Scripto', 'Transcription')); 
+        }         
 
         //set js variable for image URL
         echo '  <script>
-                    $("#transcribe-box").append("'.$metadata.'");
+                    $("#transcribe-box").append("'.$transcription.'");
                     $("#ImageID").attr("src","'.$imageUrl.'");
                 </script>'; 
     }
