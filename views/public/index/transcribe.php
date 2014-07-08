64fc8f860dfac19954a590b48c1281b579fa3dea
Matthew Butler <matthewtbutler@gmail.com>
2014-07-08 14:54:50 -0500

    cleaun up of transcription view
    moved styles to external css, replace hard-coded URL with web_root,
    merge scripts

--- views/public/index/transcribe.php
+++ views/public/index/transcribe.php
@@ -5,23 +5,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <title>DIY History</title>
-<style>	
-	html {		
-		height: 100%;	
-	}
-	body {
-		width: 100%;
-		height: 100%;
-		margin: 0px;		
-	}
-	.smooth_zoom_preloader {
-		background-image: url(../../plugins/Scriptus/views/public/index/img/preloader.gif);
-	}	
-	.smooth_zoom_icons {
-		background-image: url(../../plugins/Scriptus/views/public/index/img/icons.png);
-	}
-</style>
-
 
 <?php 
 	echo js_tag('jquery-1.9.1'); 
@@ -33,8 +16,7 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-hover-dropdown/2.0.10/bootstrap-hover-dropdown.min.js"></script>
-<link href="../../plugins/Scriptus/views/public/css/component.css" rel="stylesheet">
- 
+<link href="../../plugins/Scriptus/views/public/css/component.css" rel="stylesheet"> 
 </head>
 
 	<body class="cbp-spmenu-push">	
@@ -43,12 +25,12 @@
 
 		<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
 			
-	   		<a href="http://s-lib018.lib.uiowa.edu/omeka" alt="Home"><span class="glyphicon glyphicon-home"></span>home</a>
+	   		<a href="<?php echo WEB_ROOT; ?>" alt="Home"><span class="glyphicon glyphicon-home"></span>home</a>
 	   		<br /><br />
 	   		<ul>
-	   			<li><?php echo '<p><span class="fa fa-file-text fa-lg"></span><strong>' . $this->dc_file_title . '</strong></p>'; ?></li>
-	   			<li><?php echo '<p><span class="fa fa-book fa-lg"></span>' . $this->dc_item_link . "</p>"; ?></li>
-	   			<li><?php echo '<p><span class="fa fa-archive fa-lg"></span>' . $this->collection_link . "</p>"; ?></li>
+	   			<li><p><span class="fa fa-file-text fa-lg"></span><strong><?php echo $this->dc_file_title; ?> </strong></p></li>
+	   			<li><p><span class="fa fa-book fa-lg"></span><?php echo $this->dc_item_link; ?> </p></li>
+	   			<li><p><span class="fa fa-archive fa-lg"></span><?php echo $this->collection_link; ?> </p></li>
    			</ul>
 
    			<div class="dropdown">
@@ -73,75 +55,67 @@
 				classie.toggle( body, 'cbp-spmenu-push-toleft' );
 				classie.toggle( menuRight, 'cbp-spmenu-open' );			
 			};
-		</script>
 
-		<script>
 			jQuery(function($){
 				$('#ImageID').smoothZoom({
 					width: '100%',
 					height: '100%',
 					responsive: true
-				});				
-			});
-		</script>
+				});
 
+				$('form').submit(function(event) {
 
+						// get the form data				
+						var formData = {
+							'transcription'	: $('#transcribebox').val()
+						};
 
-		<script>
-		$('form').submit(function(event) {
+						// process the form
+						$.ajax({
+							type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+							url 		: '<?php echo Zend_Controller_Front::getInstance()->getRequest()->getRequestUri(); ?>/save', // the url where we want to POST
+							data 		: formData, // our data object
+							dataType 	: 'json', // what type of data do we expect back from the server
+				            encode          : true
+						})
+							// using the done promise callback
+							.done(function(data) {
 
-				// get the form data				
-				var formData = {
-					'transcription'	: $('#transcribebox').val()
-				};
+								// log data to the console so we can see
+								//console.log(data); 
 
-				// process the form
-				$.ajax({
-					type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-					url 		: '<?php echo Zend_Controller_Front::getInstance()->getRequest()->getRequestUri(); ?>/save', // the url where we want to POST
-					data 		: formData, // our data object
-					dataType 	: 'json', // what type of data do we expect back from the server
-		            encode          : true
-				})
-					// using the done promise callback
-					.done(function(data) {
+								// here we will handle errors and validation messages
+							});
 
-						// log data to the console so we can see
-						//console.log(data); 
-
-						// here we will handle errors and validation messages
+						// stop the form from submitting the normal way and refreshing the page
+						event.preventDefault();
 					});
 
-				// stop the form from submitting the normal way and refreshing the page
-				event.preventDefault();
+					/* simulates async login activity */
+					var doLogin = function(ms,cb) {
+					  setTimeout(function() {
+					    if(typeof cb == 'function')
+					    cb();
+					  }, ms);
+					};
+
+					$('#save-button').click(function(){
+					  var btn = $(this);
+					  
+					  btn.button("loading");
+					  btn.children().each(function(idx,ele){
+					    var icon = $(ele);
+					    icon.animate({},2000, 'linear', function() {
+					        icon.hide().fadeIn(300*idx).addClass('big');
+					     });
+					  });
+					  
+					  // perform login / async callback here
+					  doLogin(3000,function(){
+					  	btn.button("reset"); // reset button after login callback returns
+					  });	  
+					})				
 			});
-		</script>	
-
-		<script>
-		/* simulates async login activity */
-		var doLogin = function(ms,cb) {
-		  setTimeout(function() {
-		    if(typeof cb == 'function')
-		    cb();
-		  }, ms);
-		};
-
-		$('#save-button').click(function(){
-		  var btn = $(this);
-		  
-		  btn.button("loading");
-		  btn.children().each(function(idx,ele){
-		    var icon = $(ele);
-		    icon.animate({},2000, 'linear', function() {
-		        icon.hide().fadeIn(300*idx).addClass('big');
-		     });
-		  });
-		  
-		  // perform login / async callback here
-		  doLogin(3000,function(){
-		  	btn.button("reset"); // reset button after login callback returns
-		  });	  
-		})
 		</script>
 
 	</body>
